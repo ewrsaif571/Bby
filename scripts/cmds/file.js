53bd21d61f4fe4fa8a6993339703f7a7a1de0a41
SAIF ISLAM <saifewr6@gmail.com>
2025-08-09 11:18:55 +0600

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -1,36 +1,37 @@
 const fs = require('fs');
 
 module.exports = {
-	config: {
-		name: "file",
-		aliases: ["de", "give"],
-		version: "1.0",
-		author: "404",
-		countDown: 5,
-		role: 0,
-		shortDescription: "Send bot script",
-		longDescription: "Send bot specified file ",
-		category: "owner",
-		guide: "{pn} file name. Ex: .{pn} filename"
-	},
+  config: {
+    name: "file",
+    aliases: "",
+    version: "1.0",
+    author: "Mahir Tahsan",
+    countDown: 5,
+    role: 0,
+    shortDescription: "Send bot script",
+    longDescription: "Send bot specified file ",
+    category: "owner",
+    guide: "{pn} file name. Ex: .{pn} filename"
+  },
 
-	onStart: async function ({ message, args, api, event }) {
-		const permission = global.GoatBot.config.owner;
-		if (!permission.includes(event.senderID)) {
-			return api.sendMessage("Uiraa jaa mangeer pulaaa😾", event.threadID, event.messageID);
-		}
+  onStart: async function ({ message, args, api, event }) {
+    const permission = ["61577934170849" "61572403596870""61576471072700",];
+    if (!permission.includes(event.senderID)) {
+      return api.sendMessage("🙂💢", event.threadID, event.messageID);
+    }
 
-		const fileName = args[0];
-		if (!fileName) {
-			return api.sendMessage("Please provide a file name.", event.threadID, event.messageID);
-		}
+    const fileName = args[0];
+    if (!fileName) {
+      return api.sendMessage("Please provide a file name.", event.threadID, event.messageID);
+    }
 
-		const filePath = __dirname + `/${fileName}.js`;
-		if (!fs.existsSync(filePath)) {
-			return api.sendMessage(`File not found: ${fileName}.js`, event.threadID, event.messageID);
-		}
+    const filePath = __dirname + `/${fileName}.js`;
+    if (!fs.existsSync(filePath)) {
+      return api.sendMessage(`File not found: ${fileName}.js`, event.threadID, event.messageID);
+    }
 
-		const fileContent = fs.readFileSync(filePath, 'utf8');
-		api.sendMessage({ body: fileContent }, event.threadID);
-	}
+    const fileContent = fs.readFileSync(filePath, 'utf8');
+    api.sendMessage({ body: fileContent }, event.threadID);
+  }
 };
+	
